d4c8a3a4310202a9e9fc6c721613ac90bdb8098c
namhoangcaisdja <nam6cthcs@gmail.com>
2026-06-13 10:42:42 +0700

    Add .gitattributes for line endings

--- demo/src/main/java/com/example/demo/entity/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity;
-
-public enum OrderStatus {
-    NEW,
-    PROCESSING,
-    SHIPPING,
-    DELIVERED,
-    CANCELLED
-}
--- demo/src/main/java/com/example/demo/pattern/factory/AccountFactory.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.pattern.factory;
-
-import com.example.demo.entity.AccountRole;
-import com.example.demo.entity.UserAccount;
-
-public interface AccountFactory {
-    AccountRole getSupportedRole();
-    UserAccount createAccount(AccountRegistrationDTO dto);
-}
